b5f766d936abf7ad17a6dfc4837b8b83f55572b3
EunJae <ozmii333@gmail.com>
2025-12-28 02:56:49 +0900

    week4: gdb

--- practice/4.1.devenv2/math/src/main.c
+++ practice/4.1.devenv2/math/src/main.c
@@ -1,5 +1,23 @@
  #include <stdio.h>
  #include "mathops.h"
+ /*
+include/mathops.h: 함수 선언(프로토타입) 제공, 컴파일러가 타입/인자 체크 가능
+src/mathops.c: 그 함수들의 정의(실제 코드) 
+src/main.c: main()이 있고, mathops 함수들을 호출
+ */
+ /*
+ <처리 과정>
+ 1. 전처리: mathops.h 찾아서 main.c 에 붙여넣는다.
+ -> Makefile에서 CFLAGS = -Wall -O -g -Iinclude # 'include'
+ -> -Iinclude는 헤더 검색 경로에 include/ 폴더를 추가하라는 뜻.
+ 2. 컴파일: obj = src/main.o src/mathops.o
+ -> 각각에는 참조 정보와 정의 심볼이 존재함
+ 3. 링크: .o를 하나로 묶어 "실행파일 hello" 생성
+ -> $(bin): $(obj)
+		$(CC) $(obj) -o $@
+ -> 이 의미는 "cc src/main.o src/mathops.o -o hello" 와 같음
+ */
+
  int main() {
 	 int x, y;
 	 printf("두 정수를 입력하세요: ");
---
+++ practice/4.2.gdb/README.md
@@ -0,0 +1,35 @@
+# 💻 Practice: Makefile
+
+
+### 📖 Overview
+---
+(1) **Learning Make**
+
+(2) **Writing a Makefile**
+
+(3) **Writing a buildable Makefile**
+
+
+<br/>
+
+
+### 📖 개요
+---
+3주차에서 수행한 내용은 다음과 같습니다.   
+(1) **Make 배우기**
+ 
+(2) **makefile 작성 방식**
+ 
+(3) **build 가능한 Makefile 작성**
+
+
+<br/>
+
+### 📘 Example Descriptions
+
+
+### 📘 예제별 내용
+---
+각 예제에 대한 내용은 다음과 같습니다.  
+- **math**  
+- **stu**  
